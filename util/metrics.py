240e70ffce9d18f93da338f474486c25f96a1c07
Artem <apazych@students.cs.ubc.ca>
2022-04-07 13:59:38 -0700

    Reorganization stage 2

--- util/metrics.py
+++ util/metrics.py
@@ -1,1 +1,34 @@
 # This is where we have all related to the tests we’re using. (k-test, ks-test, kulback-liebler test, etc.)
+
+from scipy import stats  
+import pandas as pd
+
+
+def ks_test(real_ds, syn_ds):
+    cols = real_ds.columns() 
+    ks_tests = []
+
+    for col in cols:
+        real_ = real_ds[col]
+        syn_ = syn_ds[col]
+        try:
+            ks_tests.append({col: stats.kstest(real_, syn_)})
+        except Exception as e:
+            print(f'{e} occurred in column {col}')
+
+    return ks_tests
+
+
+def t_test(real_ds, syn_ds):
+    cols = real_ds.columns() 
+    t_tests = []
+
+    for col in cols:
+        real_ = real_ds[col]
+        syn_ = syn_ds[col]
+        try:
+            t_tests.append({col: stats.ttest_ind(real_, syn_)})
+        except Exception as e:
+            print(f'{e} occurred in column {col}')
+
+    return t_tests
--- util/preProcess.py
+++ util/preProcess.py
@@ -1,1 +1,64 @@
 # This will have any function used to clean and preprocess the dataset
+import pandas as pd
+
+real = pd.read_csv('kag_risk_factors_cervical_cancer.csv')
+
+real = real[real['Number of sexual partners'] != '?']
+real['Number of sexual partners'] = real['Number of sexual partners'].astype('float') 
+
+real = real[real['First sexual intercourse'] != '?']
+real['First sexual intercourse'] = real['First sexual intercourse'].astype('float') 
+
+real = real[real['Num of pregnancies'] != '?']
+real['Num of pregnancies'] = real['Num of pregnancies'].astype('float')
+
+real = real[real['Smokes'] != '?']
+real['Smokes'] = real['Smokes'].astype('float') 
+
+real['Smokes (years)'] = real['Smokes (years)'].astype('float') 
+
+real['Smokes (packs/year)'] = real['Smokes (packs/year)'].astype('float') 
+
+real = real[real['Hormonal Contraceptives'] != '?']
+real['Hormonal Contraceptives'] = real['Hormonal Contraceptives'].astype('float') 
+
+real['Hormonal Contraceptives (years)'] = real['Hormonal Contraceptives (years)'].astype('float') 
+
+real = real[real['IUD'] != '?']
+real['IUD'] = real['IUD'].astype('float') 
+
+real = real[real['STDs'] != '?']
+real['STDs'] = real['STDs'].astype('float') 
+
+real = real[real['STDs (number)'] != '?']
+real['STDs (number)'] = real['STDs (number)'].astype('float') 
+
+real['STDs:condylomatosis'] = real['STDs:condylomatosis'].astype('float')
+
+real['STDs:cervical condylomatosis'] = real['STDs:cervical condylomatosis'].astype('float') 
+
+real['STDs:vaginal condylomatosis'] = real['STDs:vaginal condylomatosis'].astype('float') 
+
+real['STDs:vulvo-perineal condylomatosis'] = real['STDs:vulvo-perineal condylomatosis'].astype('float') 
+
+real['STDs:syphilis'] = real['STDs:syphilis'].astype('float') 
+
+real['STDs:pelvic inflammatory disease'] = real['STDs:pelvic inflammatory disease'].astype('float') 
+
+real['STDs:genital herpes'] = real['STDs:genital herpes'].astype('float') 
+
+real['STDs:molluscum contagiosum'] = real['STDs:molluscum contagiosum'].astype('float') 
+
+real['STDs:AIDS'] = real['STDs:AIDS'].astype('float') 
+
+real['STDs:HIV'] = real['STDs:HIV'].astype('float') 
+
+real['STDs:Hepatitis B'] = real['STDs:Hepatitis B'].astype('float') 
+
+real['STDs:HPV'] = real['STDs:HPV'].astype('float') 
+
+real.drop('STDs: Time since first diagnosis', inplace=True, axis=1)
+
+real.drop('STDs: Time since last diagnosis', inplace=True, axis=1)
+
+real.to_csv('real_data.csv', index=False)
